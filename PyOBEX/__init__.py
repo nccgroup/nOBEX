18b0be8cd056059a502071565be5e89eaf9aa65c
David Boddie <david@boddie.org.uk>
2009-05-04 23:56:37 +0200

    Updated the package's version number.

--- PyOBEX/__init__.py
+++ PyOBEX/__init__.py
@@ -22,4 +22,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __all__ = ["client", "common", "headers", "requests", "responses"]
-__version__ = "0.21"
+__version__ = "0.22"
