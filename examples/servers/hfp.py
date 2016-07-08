78da70477737ccf3b7978a193357601ac3f7fea2
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2016-07-08 15:43:32 -0400

    Slightly more AT command handling
    My dummy handler may be replaced by Alex Gherman's BBeast eventually.

--- examples/servers/hfp.py
+++ examples/servers/hfp.py
@@ -2,11 +2,11 @@ import bluetooth, socket
 from PyOBEX import server
 
 class HFPMessageHandler(object):
-    def decode(self, socket_):
+    def decode(self, sock):
         msg = bytearray()
         while not msg.endswith(b'\n'):
             try:
-                msg.extend(socket_.recv(1, socket.MSG_WAITALL))
+                msg.extend(sock.recv(1, socket.MSG_WAITALL))
             except bluetooth.btcommon.BluetoothError:
                 print("connection reset")
                 break
@@ -29,5 +29,15 @@ class HFPDummyServer(server.Server):
         return server.Server.start_service(self, port, name, uuid, service_classes,
                 service_profiles, provider, description, protocols)
 
-    def process_request(self, connection, cmd):
+    def process_request(self, sock, cmd):
         print("received AT cmd: %s" % cmd)
+        if cmd.startswith(b'AT+BRSF'):
+            self._reply(sock, b'+BRSF: 0\r\n')
+        else:
+            self._reply(sock, b'OK\r\n')
+
+    def _reply(self, sock, resp):
+        try:
+            sock.sendall(resp)
+        except:
+            print("failure writing AT cmd response")
