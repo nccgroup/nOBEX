c59578e895ad2c216910c4c69c2e81de4fa43dc7
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2016-07-19 13:55:45 -0400

    Allow specifying HFP config file as argument

--- examples/hfpserver.py
+++ examples/hfpserver.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
+import sys
 from servers import hfp
 
-def run_server(device_address):
+def run_server(config_file=None):
     # Run the server in a function so that, if the server causes an exception
     # to be raised, the server instance will be deleted properly, giving us a
     # chance to create a new one and start the service again without getting
     # errors about the address still being in use.
-    server = hfp.HFPServer(device_address)
+    server = hfp.HFPServer(config_file)
     socket = server.start_service()
 
     try:
@@ -17,5 +18,7 @@ def run_server(device_address):
 
 
 if __name__ == "__main__":
+    cf = None
+    if len(sys.argv) > 1: cf = sys.argv[1]
     while True:
-        run_server("")
+        run_server(cf)
