829d700324968bd6097cadc9817b643bbaf1bac9
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2017-12-18 11:35:43 -0500

    examples: whitespace cleanup

--- examples/get_files.py
+++ examples/get_files.py
@@ -15,57 +15,53 @@ from xml.etree import ElementTree
 from nOBEX import client, responses
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 3:
-    
         sys.stderr.write("Usage: %s <device address> <directory>\n" % sys.argv[0])
         sys.exit(1)
-    
+
     device_address = sys.argv[1]
     path = sys.argv[2]
-    
+
     services = bluetooth.find_service(uuid="1106", address=device_address)
     if services:
         port = services[0]["port"]
-    
+
     c = client.BrowserClient(device_address, port)
-    
+
     response = c.connect()
     if not isinstance(response, responses.ConnectSuccess):
         sys.stderr.write("Failed to connect.\n")
         sys.exit(1)
-    
+
     pieces = path.split("/")
-    
+
     for piece in pieces:
-    
         response = c.setpath(piece)
         if isinstance(response, responses.FailureResponse):
             sys.stderr.write("Failed to enter directory.\n")
             sys.exit(1)
-    
+
     sys.stdout.write("Entered directory: %s\n" % path)
-    
+
     response = c.listdir()
-    
+
     if isinstance(response, responses.FailureResponse):
         sys.stderr.write("Failed to list directory.\n")
         sys.exit(1)
-    
+
     headers, data = response
     tree = ElementTree.fromstring(data)
     for element in tree.findall("file"):
-    
         name = element.attrib["name"]
-        
+
         if os.path.exists(name):
             sys.stderr.write("File already exists: %s\n" % name)
             continue
-        
+
         sys.stdout.write("Fetching file: %s\n" % name)
-        
+
         response = c.get(name)
-        
+
         if isinstance(response, responses.FailureResponse):
             sys.stderr.write("Failed to get file: %s\n" % name)
         else:
@@ -75,6 +71,6 @@ if __name__ == "__main__":
                 open(name, "wb").write(data)
             except IOError:
                 sys.stderr.write("Failed to write file: %s\n" % name)
-    
+
     c.disconnect()
     sys.exit()
--- examples/threaded_connection.py
+++ examples/threaded_connection.py
@@ -18,22 +18,17 @@ import bluetooth, sys, threading
 from nOBEX import client, responses
 
 class CallerException(Exception):
-
     pass
-    
-class Caller:
 
+class Caller:
     def __init__(self, timeout = 10):
-    
         self.timeout = timeout
-    
+
     def _call(self, fn, args):
-    
         self.result = fn(*args)
         self.has_result = True
-    
+
     def __call__(self, fn, args = ()):
-    
         self.has_result = False
         thread = threading.Thread(target = self._call, args = (fn, args))
         thread.start()
@@ -45,31 +40,30 @@ class Caller:
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 2:
         sys.stderr.write("Usage: %s <device bluetooth address>\n" % sys.argv[0])
         sys.exit(1)
-    
+
     device_address = sys.argv[1]
-    
+
     services = bluetooth.find_service(uuid="1106", address=device_address)
     if not services:
         sys.stderr.write("No file transfer service on the device.\n")
         sys.exit(1)
-    
+
     port = services[0]["port"]
-    
+
     c = client.BrowserClient(device_address, port)
-    
+
     call = Caller()
-    
+
     try:
         result = call(c.connect)
         if isinstance(result, responses.ConnectSuccess):
             print(call(c.listdir))
             call(c.disconnect)
-    
+
     except CallerException:
         sys.stderr.write("Connection timed out.\n")
-    
+
     sys.exit()
