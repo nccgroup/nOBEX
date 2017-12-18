84dec35f9f1e6da74b9d194e282d888c52e2a5f6
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2017-12-18 18:38:24 -0500

    bluez_helper: use sdptool to advertise and find services
    The PyBluez implementation doesn't give me full control of the SDP
    record, and so for some profiles (like HFP), I miss fields in the
    SDP record that are imporant to some clients. Using sdptool does
    set all the fields I need, so I'll be switching to it.

---
+++ nOBEX/bluez_helper.py
@@ -0,0 +1,55 @@
+import socket, subprocess, sys
+import xml.etree.ElementTree as ET
+
+# need to make sure this works
+def BluetoothSocket():
+    return socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
+            socket.BTPROTO_RFCOMM)
+
+# We are using the deprecated Bluez "Service" API instead of the new "Profile"
+# API since the "Service" API works more the way I want it to.
+# sdptool is the easy/lazy way to use this API without native code
+
+class SDPException(Exception):
+    pass
+
+def advertise_service(name, channel):
+    val = subprocess.run(["sdptool", "add", "--channel=%i" % channel, name],
+            stdout=subprocess.PIPE)
+    if val.returncode != 0:
+        raise SDPException("sdptool add returned %i" % val.returncode)
+
+def stop_advertising(name):
+    h, c = _search_record(name, "local")
+    val = subprocess.run(["sdptool", "del", h], stdout=subprocess.PIPE)
+    if val.returncode != 0:
+        raise SDPException("sdptool del returned %i" % val.returncode)
+
+def find_service(name, bdaddr):
+    h, c = _search_record(name, bdaddr)
+    return c
+
+def _search_record(name, bdaddr):
+    val = subprocess.run(
+            ["sdptool", "search", "--xml", "--bdaddr=%s" % bdaddr, name],
+            stdout=subprocess.PIPE)
+    if val.returncode != 0:
+        raise SDPException("sdptool search returned %i" % val.returncode)
+
+    # Strip out first line that's not XML
+    xml_str = b'\n'.join(val.stdout.splitlines()[1:])
+    try:
+        tree = ET.fromstring(xml_str)
+    except ET.ParseError:
+        raise SDPException("Service %s not found on %s" % (name, bdaddr))
+
+    # this code is probably fragile
+    handle = _find_attr(tree, "0x0000")[0].attrib["value"]
+    channel = int(_find_attr(tree, "0x0004")[0][1][1].attrib["value"], 16)
+    return handle, channel
+
+def _find_attr(xml_tree, attr_id):
+    for elem in xml_tree:
+        if elem.tag == "attribute" and elem.attrib["id"] == attr_id:
+            return elem
+    raise SDPException("Attribute %s not found!" % attr_id)
